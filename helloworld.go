e6631081bb81febcd66671f20a05fc902f821c2b
joshua <joshuaaguilar20@gmail.com>
2019-06-17 13:29:07 -0700

    first commit

--- helloworld.go
+++ helloworld.go
@@ -3,10 +3,13 @@ import "fmt"
 
 
 func main(){
+	/* Creating a Slice ( array that grows and shrinks) */
 	cards := []string{"Ace of Diamonds", newCard()}
+	/* Append Creates new Arrary and Appends Item */
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
+		/* Loops over Slice to Print each Card */
 		fmt.Println(i, card)
 	}
 }
